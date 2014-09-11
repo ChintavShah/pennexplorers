33630ab4c4f8092be4d5012a422b18b9d76e9cfc
Chintav Shah <sword4life4@gmail.com>
2014-09-11 01:47:50 -0400

    pennexplorers

---
+++ pennexplorers/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pennexplorers::Application.config.secret_key_base = '0933ca3c00c148375bf82efe7f2b4014d4af4ccdcfb0b55238185acaf6e9e68ffc3cc451a5461a91a679eb08efc3d72684ce8f168199909a24270aa89dd65e56'
---
+++ pennexplorers/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pennexplorers::Application.config.session_store :cookie_store, key: '_pennexplorers_session'
